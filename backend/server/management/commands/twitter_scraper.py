93ed64de900cc4667de6626a96afdca026024ae3
unknown <liangkathleen@gmail.com>
2021-05-01 16:46:35 -0400

    get name from tweet and run every hour

--- backend/server/management/commands/twitter_scraper.py
+++ backend/server/management/commands/twitter_scraper.py
@@ -1,35 +1,44 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from pytwitterscraper import TwitterScraper
-import datetime
-import pytz
-import re
+import datetime, time, pytz, re
 
 class Command(BaseCommand):
     help = 'Scrapes @VaxHuntersCan for hotspot vaccine clinics'
 
     def postal_codes(self, tweet):
-        return re.findall("[ML][0-9][A-Z]", tweet)
+        postal_codes = re.findall("[ML][0-9][A-Z]", tweet)
+        if len(postal_codes) > 0:
+            return postal_codes
+        return False
     
     def age_restrictions(self, tweet):
         ages = re.findall("[1-9][0-9][+]", tweet)
-        if ages:
+        if len(ages) > 0:
             return ages[0][:-1]
         return 18
     
     def address(self, tweet):
         address = re.findall("\\d+[ ](?:[a-zA-Z]+[ ])+(?:Street|St|Avenue|Ave|Road|Rd|Highway|Hwy|Square|Sq|trail|Trl|Drive|Dr|Court|Ct|Parkway|Pkwy|Circle|Cir|Boulevard|Blvd)", tweet)
-        if address:
+        if len(address) > 0:
             return address[0]
         return False
 
-    def valid_tweet(self, tweet, created):
+    def name(self, tweet):
+        name = re.findall("at (?:[A-Z][a-z.]+[ ])+(?:Arena|Centre|PS|Clinic|clinic)", tweet)
+        if len(name) > 0:
+            return name[0][3:]
+        name = re.findall("ONLY(?:[A-Z][a-z.]+[ ])+(?:Arena|Centre|PS|Clinic|clinic)", tweet)
+        if len(name) > 0:
+            return name[0][4:]
+        return ''
+
+    def valid_tweet(self, tweet, created, start_time):
         utc=pytz.UTC
-        # only want tweets from May 1st onward
-        if created < utc.localize(datetime.datetime(2021, 5, 1, 0, 0, 0)): return False
-        # TODO: get the time of previous run and ignore tweets before that
+
+        if created < utc.localize(start_time): return False
         if '[ON]' not in tweet: return False
-        if len(self.postal_codes(tweet)) <= 0: return False
+        if not self.postal_codes(tweet): return False
         if not self.address(tweet): return False
 
         return True
@@ -38,27 +47,35 @@ class Command(BaseCommand):
         address = self.address(tweet)
         postal_codes = self.postal_codes(tweet)
         age_group = self.age_restrictions(tweet)
-        return address, postal_codes, age_group
+        name = self.name(tweet)
+        return address, postal_codes, age_group, name
 
     def handle(self, *args, **kwargs):
         VAX_HUNTERS_CAN_ID = 1373531468744552448
-        time = timezone.now().strftime('%X')
+        # only consider tweets May 1 and onwards
+        start_time = datetime.datetime(2021, 5, 1, 0, 0, 0)
 
-        ts = TwitterScraper()
-        tweets = ts.get_tweets(VAX_HUNTERS_CAN_ID, count=300)
+        while True:
+            now = datetime.datetime.now()
 
-        text = []
-        urls = []
-        created_at = []
+            ts = TwitterScraper()
+            tweets = ts.get_tweets(VAX_HUNTERS_CAN_ID, count=300)
 
-        for tweet in tweets.contents:
-            text.append(tweet['text'])
-            urls.append(tweet['urls'][0]['url']) if len(tweet['urls']) > 0 else urls.append('')
-            created_at.append(tweet['created_at'])
+            text = []
+            urls = []
+            created_at = []
 
-        for (tweet, url, created) in zip(text, urls, created_at):
-            if not self.valid_tweet(tweet, created): continue
-    
-            address, postal_codes, age_group = self.parse_tweet(tweet)
-            # TODO: insert into db
+            for tweet in tweets.contents:
+                text.append(tweet['text'])
+                urls.append(tweet['urls'][0]['url']) if len(tweet['urls']) > 0 else urls.append('')
+                created_at.append(tweet['created_at'] - datetime.timedelta(hours=4))
+
+            for (tweet, url, created) in zip(text, urls, created_at):
+                if not self.valid_tweet(tweet, created, start_time): continue
+        
+                address, postal_codes, age_group, name = self.parse_tweet(tweet)
+                # TODO: insert into db
+            start_time = now
+            time.sleep(60)
+
             
